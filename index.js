fb6f3e5bd5416398d938d2d756065ecffb0137bc
aveen007 <aveen2000hussein@gmail.com>
2022-06-01 10:05:57 +0200

    added setTimeout

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
+const rectangle = require("./rectangle")
 var rect=require("./rectangle")
 
 function solveRectangle(l,b){
     console.log("solving for l="+l+" and b= "+ b)
-    if (l<=0||b<=0){
+    // if (l<=0||b<=0){
 
-        console.log("Rectangle dimensions should be greater than zero")
-    }
-    else{
-        console.log("Rectangle area is"+ rect.area(l,b))
-        console.log("Rectangle perimeter is "+rect.perimeter(l,b))
-
-
-    }
+    //     console.log("Rectangle dimensions should be greater than zero")
+    // }
+    // else{
+    //     console.log("Rectangle area is"+ rect.area(l,b))
+    //     console.log("Rectangle perimeter is "+rect.perimeter(l,b))
 
 
+    // }
+rect(l,b,(err,rectangle)=>{if (err)
+console.log("ERROR: "+err.message)
+else{
+    console.log("Rectangle area is"+ rectangle.area())
+        console.log("Rectangle perimeter is "+rectangle.perimeter())
+
+    
+}
+}
+);
+console.log("that's after")
 }
 solveRectangle(2,4)
 solveRectangle(-2,4)
